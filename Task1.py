12192cce18536c6d3ea5ffa38e1c8d87173724e6
sharmas1-git <sameersharma0390@gmail.com>
2026-02-08 12:16:06 +0530

    Initial commit

---
+++ Task1.py
@@ -0,0 +1,24 @@
+# Task 1: Perform Basic Mathematical Operations
+# In this task we will take twp float values num1 & num2 with input function to take user values
+# Add= num1 + num2####
+# Substraction = num2 - num1
+# Multiplication = num1 * num2####
+# Division = num 2 / num1#####
+
+
+num1= float(input("Enter num1 value: "))
+num2 = float(input("Enter num2 value: "))
+
+Addition = (num1 + num2)
+Substraction = (num2 - num1)
+Multiplication = (num1 * num2)
+Division = (num1 / num2)
+
+print("Addition:", Addition)
+print("Substraction:", Substraction)
+print("Multiplication:", Multiplication)
+print("Division:", Division)
+
+
+
+
---
+++ Task2.py
@@ -0,0 +1,10 @@
+# Here we will make use of String Concatenation.
+# We will take inputs in string values.
+# Str1=  Enter your first name
+# Str2= Enter you last name
+# then we print the concatenated output
+
+str1=str(input("Enter your first name:"))
+str2=str(input("Enter your last name:"))
+Result= str1 + str2
+print ("Hello,", Result, "!Welcome to the Python program" )
